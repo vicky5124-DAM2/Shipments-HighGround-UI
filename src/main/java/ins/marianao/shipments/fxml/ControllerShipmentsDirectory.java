c9d3aadb180eca5daba797616a6a2fd6fca00069
marcrebullida2004 <marcrebullidalozano@gmail.com>
2024-02-19 20:49:22 +0100

    He hecho ViewFormShipment.fxml, ViewShipmentsDirectory.fxml y he creado ControllerFormShipment (el controller está por empezar).

--- src/main/java/ins/marianao/shipments/fxml/ControllerShipmentsDirectory.java
+++ src/main/java/ins/marianao/shipments/fxml/ControllerShipmentsDirectory.java
@@ -1,4 +1,31 @@
 package ins.marianao.shipments.fxml;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ControllerShipmentsDirectory {
+    @FXML
+    private TableView<Shipments> shipmentsTable;
+    @FXML
+    private TableColumn<Shipments, String> colAddresseeName;
+
+    @FXML
+    private TableColumn<Shipments, String> colAssignedCourier;
+
+    @FXML
+    private TableColumn<Shipments, Integer> colCategory;
+
+    @FXML
+    private TableColumn<Shipments, Boolean> colDelivered;
+
+    @FXML
+    private TableColumn<Shipments, Integer> colPriority;
+
+    @FXML
+    private TableColumn<Shipments, String> colRecipientAddress;
+
+    @FXML
+    private TableColumn<Shipments, Integer> colTrackingId;
+
 }
